855947b11d2d0b009f12f91dc871d97716e33948
frontendsprint <frontendsprint@gmail.com>
2023-12-07 13:50:48 +0200

    commit

---
+++ 8kyu/neutralisation.js
@@ -0,0 +1,27 @@
+// DESCRIPTION:
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+//SOLUTION:
+
+function neutralise(s1, s2) {
+    let arr = [];
+    let result = '';
+    for (let i = 0; i < s1.length; i++){
+      if (s1[i] === s2[i]) {
+        arr.push(s1[i]);
+      } else {
+        arr.push(0);
+      }
+      result = arr.join('');
+    }
+    // Here be dragons!
+    return result;
+}
+
+//RESULT
+// ("--++--", "++--++") ➞ "000000"
+// ("-+-+-+", "-+-+-+") ➞ "-+-+-+"
+// ("-++-", "-+-+") ➞ "-+00"
