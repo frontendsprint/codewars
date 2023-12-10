969a6c3a21082b87e6bdde79f4d8fb7f26f0565e
frontendsprint <frontendsprint@gmail.com>
2023-12-10 12:11:03 +0200

    commit

---
+++ 8kyu/find-numbers-which-are-divisible-by-given-number.js
@@ -0,0 +1,9 @@
+function divisibleBy(numbers, divisor){
+    let result = [];
+    for (let i = 0; i < numbers.length; i++) {
+     if (numbers[i] % divisor === 0) {
+       result.push(numbers[i]);
+     }
+   }
+    return result;
+  }
